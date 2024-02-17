090461ac1bfaecaee60ed10fb78c82c3ddcb07fe
tc6386 <tc6386@rit.edu>
2024-02-17 16:58:11 -0500

    finished second bag

--- sdpc-spin/main.py
+++ sdpc-spin/main.py
@@ -13,32 +13,33 @@ FIRST_J = False
 FIRST_Z = False
 FIRST_I = False
 DELAYED_RED = False
+SECOND_BAG = {"O":False,"S":False,"L":False,"J":False,"Z":False,"I":False}
 def press_left():
     pyautogui.press("left")
-    time.sleep(random.random()*0.05)
+    time.sleep(random.random()*0.005)
 
 def press_up():
     pyautogui.press("up")
-    time.sleep(random.random()*0.1)
+    time.sleep(random.random()*0.003)
 def press_right():
     pyautogui.press("right")
-    time.sleep(random.random()*0.05)
+    time.sleep(random.random()*0.005)
 
 def press_space():
     pyautogui.press("space")
-    time.sleep(random.random() * 0.05)
+    time.sleep(random.random() * 0.005)
 
 def press_down():
     pyautogui.keyDown("down")
-    time.sleep(random.random() * 0.1)
+    time.sleep(random.random() * 0.003)
     pyautogui.keyUp("down")
-    time.sleep(random.random() * 0.07)
+    time.sleep(random.random() * 0.007)
 def press_z():
     pyautogui.press("z")
-    time.sleep(random.random() * 0.07)
+    time.sleep(random.random() * 0.004)
 def press_c():
     pyautogui.press("c")
-    time.sleep(random.random() * 0.06)
+    time.sleep(random.random() * 0.002)
 
 def first_bag():
     global FIRST_I, FIRST_J, FIRST_L, FIRST_Z, FIRST_S, FIRST_O, DELAYED_RED
@@ -49,7 +50,6 @@ def first_bag():
             for _ in range(6):
                 press_right()
             press_space()
-            time.sleep(0.5)
             return 0
     except:
         pass
@@ -68,7 +68,6 @@ def first_bag():
             for _ in range(5):
                 press_left()
             press_space()
-            time.sleep(0.5)
             return 0
     except:
         pass
@@ -83,7 +82,6 @@ def first_bag():
             if FIRST_S != True:
                 press_right()
             press_space()
-            time.sleep(0.5)
             return 0
     except:
         pass
@@ -91,9 +89,8 @@ def first_bag():
         if pyautogui.locateOnScreen('../images/L.png', region=(825, 70, 300, 100), confidence=0.8) != None:
             FIRST_L = True
             press_z()
-            for _ in range(6):
+            for _ in range(4):
                 press_right()
-            press_left()
             press_space()
             return 0
     except:
@@ -110,7 +107,6 @@ def first_bag():
             for _ in range(5):
                 press_left()
             press_space()
-            time.sleep(0.5)
             return 0
     except:
         pass
@@ -129,13 +125,128 @@ def first_T_Drop():
     press_down()
     press_up()
     press_space()
+
+def second_bag():
+    global SECOND_BAG
+    try:
+        if pyautogui.locateOnScreen('../images/s.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["S"] = True
+            press_up()
+            press_down()
+            press_z()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/L.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["L"] = True
+            press_up()
+            for _ in range(4):
+                press_right()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/T.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            press_c()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/O.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["O"] = True
+            for _ in range(3):
+                press_left()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/J.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["J"] = True
+            for _ in range(2):
+                press_right()
+            press_down()
+            press_right()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/I.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["I"] = True
+            press_z()
+            for _ in range(4):
+                press_left()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/Z.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["Z"] = True
+            press_z()
+            press_down()
+            press_left()
+            press_space()
+            return 0
+    except:
+        pass
+def second_T_Drop():
+    press_up()
+    press_down()
+    press_up()
+    press_space()
+
+def third_bag():
+    try:
+        if pyautogui.locateOnScreen('../images/I.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+
+            press_z()
+            for _ in range(5):
+                press_right()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/L.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            press_z()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/J.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            press_up()
+            press_right()
+            press_space()
+            return 0
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen('../images/S.png', region=(825, 70, 300, 100), confidence=0.7) != None:
+            SECOND_BAG["Z"] = True
+            press_z()
+            for _ in range(4):
+                press_right()
+            press_space()
+            return 0
+    except:
+        pass
 def main():
     global FIRST_I, FIRST_J, FIRST_L, FIRST_Z, FIRST_S, FIRST_O
     time.sleep(3)
-    while FIRST_I!=True or FIRST_J!=True or FIRST_L!=True or FIRST_Z!=True or FIRST_S!=True or FIRST_O!=True:
+    while FIRST_I!= True or FIRST_J != True or FIRST_L != True or FIRST_Z != True or FIRST_S != True or FIRST_O != True:
         first_bag()
     first_T_Drop()
-
+    while SECOND_BAG["O"]!= True or SECOND_BAG["J"] != True or SECOND_BAG["L"] != True or SECOND_BAG["Z"] != True or SECOND_BAG["S"] != True or SECOND_BAG["I"] != True:
+        second_bag()
+    second_T_Drop()
+    while 1:
+        third_bag()
 
 
 if __name__ == "__main__":
