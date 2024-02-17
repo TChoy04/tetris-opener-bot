b23d452d27938e99977657ca958c6965273718f8
tc6386 <tc6386@rit.edu>
2024-02-17 00:00:20 -0500

    automated first block placement for o

--- sdpc-spin/main.py
+++ sdpc-spin/main.py
@@ -5,19 +5,35 @@ import keyboard
 import random
 import random
 import win32api, win32con
+import pydirectinput
 
+def press_left():
+    pyautogui.press("left")
+    time.sleep(random.random()*0.1)
 
+def press_space():
+    pyautogui.press("space")
+    time.sleep(random.random()*0.15)
+def first_bag():
+
+    try:
+        if pyautogui.locateOnScreen('../images/O.png',confidence=0.9) != None:
+            pyautogui.press("c")
+            press_left()
+            press_left()
+            press_left()
+            press_left()
+            press_space()
+
+    except:
+        print("bye")
+        time.sleep(1)
+    return 0
 def main():
-    while 1:
-        try:
-            if pyautogui.locateOnScreen('../images/T.png')!=None:
-                print("hello")
-                pyautogui.press("z")
-                time.sleep(1)
+    time.sleep(3)
+    while(1):
+        first_bag()
 
-        except:
-            print("bye")
-            time.sleep(1)
 
 if __name__ == "__main__":
     main()
