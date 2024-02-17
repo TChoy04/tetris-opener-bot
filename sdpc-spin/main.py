552b007f3432de1daefad84cc99d65cfc5ce1d4d
tc6386 <tc6386@rit.edu>
2024-02-16 23:36:54 -0500

    added basic image recognition

--- sdpc-spin/main.py
+++ sdpc-spin/main.py
@@ -5,3 +5,17 @@ import keyboard
 import random
 import random
 import win32api, win32con
+
+
+def main():
+    while 1:
+        try:
+            if pyautogui.locateOnScreen('../images/T.png')!=None:
+                print("hello")
+                time.sleep(2)
+        except:
+            print("bye")
+            time.sleep(2)
+
+if __name__ == "__main__":
+    main()
